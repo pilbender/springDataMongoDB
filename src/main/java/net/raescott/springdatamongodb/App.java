abebf393d8d43d610e065e553d994c78ba57bd80
Richard Scott Smith <scott@thescene.com>
2012-05-02 13:58:52 -0700

    Initial import of a Spring Data project using MongoDB.

---
+++ src/main/java/net/raescott/springdatamongodb/App.java
@@ -0,0 +1,12 @@
+package net.raescott.springdatamongodb;
+
+/**
+ * MongoDB with Spring Data and Entity Beans.
+ *
+ */
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
