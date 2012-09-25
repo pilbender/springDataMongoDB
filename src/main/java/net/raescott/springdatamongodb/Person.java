4d005ca42e96c6a68fdf5fb61da41a14e5ebc4fe
Richard Scott Smith <scott.smith@isostech.com>
2012-09-25 12:02:34 -0700

    Added the Person Entity to the Mongo data store.

---
+++ src/main/java/net/raescott/springdatamongodb/Person.java
@@ -0,0 +1,65 @@
+package net.raescott.springdatamongodb;
+
+/**
+ * Entity Bean.
+ *
+ * @author Richard Scott Smith <scott.smith@isostech.com>
+ */
+public class Person {
+
+	private String id;
+	private String name;
+	private int age;
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+}
